9ad610ecc4430a4787565d6e2c5df29b2346fe33
Ankit jhunjhunwala <ankit.jhunjhunwala22@gmail.com>
2014-10-30 03:53:46 +0530

    Create initialize.h

---
+++ initialize.h
@@ -0,0 +1,16 @@
+#ifndef INITIALIZE_H
+#define INITIALIZE_H
+#include<stdlib.h>
+#include "blacksoldier.h"
+#include "whitesoldier.h"
+#include "blackking.h"
+#include "whiteking.h"
+#include "blackqueen.h"
+#include "whitequeen.h"
+#include "blackelephant.h"
+#include "whiteelephant.h"
+#include "blackuut.h"
+#include "whiteuut.h"
+#include "blackhorse.h"
+#include "whitehorse.h"
+#endif // INITIALIZE_H
